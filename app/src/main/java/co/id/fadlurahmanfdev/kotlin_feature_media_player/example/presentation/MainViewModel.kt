dff14ee1768c890b6b0f51155ef3c068dce09a0a
fadlurahmanf <tffajari@gmail.com>
2024-05-25 18:05:49 +0700

    fix

---
+++ app/src/main/java/co/id/fadlurahmanfdev/kotlin_feature_media_player/example/presentation/MainViewModel.kt
@@ -0,0 +1,10 @@
+package co.id.fadlurahmanfdev.kotlin_feature_media_player.example.presentation
+
+import androidx.lifecycle.ViewModel
+import co.id.fadlurahmanfdev.kotlin_feature_media_player.example.domain.ExampleMediaPlayerUseCase
+
+class MainViewModel(
+    private val exampleMediaPlayerUseCase: ExampleMediaPlayerUseCase
+) : ViewModel() {
+
+}
