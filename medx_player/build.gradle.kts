544af32e8b6d94e5ddf541394382438741a6a376
fadlurahmanfdev <taufikfadlurahmanfajari.dev@gmail.com>
2025-11-10 17:18:08 +0700

    feat(medx-player): increment version medx_player

--- medx_player/build.gradle.kts
+++ medx_player/build.gradle.kts
@@ -56,7 +56,7 @@ mavenPublishing {
     publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
     signAllPublications()
 
-    coordinates("com.fadlurahmanfdev", "medx_player", "0.0.1")
+    coordinates("com.fadlurahmanfdev", "medx_player", "0.0.2")
 
     pom {
         name.set("Medx Player")
